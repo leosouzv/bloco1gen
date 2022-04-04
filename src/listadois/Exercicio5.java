f6d26708a426c5bc6747bfd0106eba63b9e25a49
leosouzv <leogrdss@icloud.com>
2022-04-04 15:51:23 -0300

    Adding exercise 5

---
+++ src/listadois/Exercicio5.java
@@ -0,0 +1,23 @@
+package listadois;
+
+import java.util.Scanner;
+
+public class Exercicio5 {
+
+	static int numero, soma = 0;
+
+	public static void main(String args[]) {
+
+		Scanner scan = new Scanner(System.in);
+
+		do {
+			System.out.println("Digite um número: ");
+			numero = scan.nextInt();
+			soma += numero;
+
+		} while (numero != 0);
+
+		System.out.println("A soma dos número digitados é: " + soma);
+
+	}
+}
