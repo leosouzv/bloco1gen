bf01dd959ad6fb6ef6fb8e206e288d1e55f20d8e
leosouzv <leogrdss@icloud.com>
2022-04-06 14:08:00 -0300

    Produto Eletronico

--- src/listatres/ModeloProdutoEletronico.java
+++ src/listatres/ModeloProdutoEletronico.java
@@ -1,9 +1,30 @@
 package listatres;
 
 public class ModeloProdutoEletronico {
+	public String marca, modelo, so, aparelho;
+	public boolean funciona, garantia;
+
+	public void funcionamento() {
+		if (funciona == true) {
+			System.out.println("O aparelho está funcionando!");
+		} else {
+			System.out.println("O aparelho não está funcionando!");
+		}
+
+	}
+	
+	public void garantia(){
+		if(garantia==true) {
+			System.out.println("O aparelho possui garantia!");
+		} else {
+			System.out.println("O aparelho não possui garantia!");
+		}
+		
+	}
 
 	public void estado() {
-		
-		
+
+		System.out.println("O produto é: " + this.marca +" "+ this.aparelho +" "+ this.modelo);
+		System.out.println("O Sistema Operacional é: " + this.so);
 	}
 }
--- src/listatres/ProdutoEletronico.java
+++ src/listatres/ProdutoEletronico.java
@@ -3,8 +3,19 @@ package listatres;
 public class ProdutoEletronico {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ModeloProdutoEletronico p1 = new ModeloProdutoEletronico();
+		
+		p1.marca="Apple";
+		p1.aparelho="Iphone";
+		p1.modelo="XR";
+		p1.so="IOS";
+		p1.funciona=true;
+		p1.garantia=true;
+		
+		
+		p1.estado();
+		p1.garantia();
+		p1.funcionamento();
 	}
 
 }
